985538a7f62b18b20d241749d7f9ef0d11df10f6
hoetmaaiers <robin.houdmeyers@icapps.com>
2017-12-04 19:01:05 +0100

    Add day 4 solutions

---
+++ puzzle-4.ex
@@ -0,0 +1,30 @@
+defmodule Puzzle do
+  @input File.read!("input/day_4.txt")
+  @line_sep "\n"
+  @word_sep " "
+
+  def solve() do
+    @input
+    |> parse
+    |> Enum.map(&valid_passphrase(&1))
+    |> Enum.filter(fn(x) -> x end)
+    |> length
+    |> IO.inspect
+  end
+
+  defp parse(input) do
+    input
+    |> String.splitter(@line_sep, trim: true)
+    |> Enum.map(fn(line) ->
+      line
+      |> String.split(@word_sep)
+    end)
+  end
+
+  def valid_passphrase(phrase) do
+    length(Enum.uniq(phrase)) == length(phrase)
+  end
+
+end
+
+Puzzle.solve()
---
+++ puzzle-4_part2.ex
@@ -0,0 +1,40 @@
+defmodule Puzzle do
+  @input File.read!("input/day_4.txt")
+  @line_sep "\n"
+  @word_sep " "
+
+  def solve() do
+    @input
+    |> parse
+    |> Enum.map(&valid_passphrase(&1))
+    |> Enum.filter(fn(x) -> x end)
+    |> length
+    |> IO.inspect
+  end
+
+  def valid_passphrase(phrase) do
+    uniq_phrase = phrase
+    |> Enum.map(&az_order(&1))
+    |> Enum.uniq
+
+    length(uniq_phrase) == length(phrase)
+  end
+
+  defp az_order(word) do
+      word
+      |> String.split("", trim: true)
+      |> Enum.sort
+      |> List.to_string
+  end
+
+  defp parse(input) do
+    input
+    |> String.splitter(@line_sep, trim: true)
+    |> Enum.map(fn(line) ->
+      line
+      |> String.split(@word_sep)
+    end)
+  end
+end
+
+Puzzle.solve()
